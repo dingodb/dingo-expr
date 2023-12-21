364752c687767470bc590e914b79bde09a1e39b9
Yueguang Jiao <jiaoyg@zetyun.com>
2023-12-21 11:33:23 +0800

    Add test cases for rel op coder.

--- coding/src/test/java/io/dingodb/expr/coding/TestRelOpCoder.java
+++ coding/src/test/java/io/dingodb/expr/coding/TestRelOpCoder.java
@@ -20,23 +20,54 @@ import io.dingodb.expr.parser.exception.ExprParseException;
 import io.dingodb.expr.rel.RelConfig;
 import io.dingodb.expr.rel.RelOp;
 import io.dingodb.expr.rel.op.RelOpStringBuilder;
+import io.dingodb.expr.runtime.type.TupleType;
 import io.dingodb.expr.runtime.type.Types;
 import io.dingodb.expr.runtime.utils.CodecUtils;
-import org.junit.jupiter.api.Test;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.ByteArrayOutputStream;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class TestRelOpCoder {
-    @Test
-    public void test() throws ExprParseException {
-        RelOp op = RelOpStringBuilder.builder(RelConfig.DEFAULT)
-            .filter("$[2] > 50")
-            .build();
-        op.init(Types.tuple("INT", "STRING", "FLOAT"), RelConfig.DEFAULT);
+    private static @NonNull Stream<Arguments> getParameters() throws ExprParseException {
+        return Stream.of(
+            arguments(
+                RelOpStringBuilder.builder(RelConfig.DEFAULT)
+                    .filter("$[2] > 50")
+                    .build(),
+                Types.tuple("INT", "STRING", "FLOAT"),
+                "7134021442480000930400"
+            ),
+            arguments(
+                RelOpStringBuilder.builder(RelConfig.DEFAULT)
+                    .project("$[0]", "$[1]", "$[2] / 10")
+                    .build(),
+                Types.tuple("INT", "STRING", "FLOAT"),
+                "726F0331000037010034021441200000860400"
+            ),
+            arguments(
+                RelOpStringBuilder.builder(RelConfig.DEFAULT)
+                    .filter("$[2] > 50")
+                    .project("$[0]", "$[1]", "$[2] / 10")
+                    .build(),
+                Types.tuple("INT", "STRING", "FLOAT"),
+                "7134021442480000930400726F0331000037010034021441200000860400"
+            )
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("getParameters")
+    public void test(@NonNull RelOp op, TupleType type, String code) {
+        op.init(type, RelConfig.DEFAULT);
         ByteArrayOutputStream os = new ByteArrayOutputStream();
         RelOpCoder.INSTANCE.visit(op, os);
-        assertThat(os.toByteArray()).isEqualTo(CodecUtils.hexStringToBytes("7134021442480000930400"));
+        assertThat(os.toByteArray()).isEqualTo(CodecUtils.hexStringToBytes(code));
     }
 }
